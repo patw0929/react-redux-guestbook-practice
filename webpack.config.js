2437e75255366a61f0a14cd519c4e6483af520ad
patw <patw.hi@gmail.com>
2015-09-08 17:57:25 +0800

    Webpack & server configuration

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/*
+ * Webpack development server configuration
+ *
+ * This file is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
+ */
+
+var webpack = require('webpack'),
+    path = require('path');
+var eslintrcPath = path.resolve(__dirname, '.eslintrc');
+
+module.exports = {
+  devtool: 'eval',
+  watch: true,
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index',
+  ],
+
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'styles': __dirname + '/src/styles',
+      'mixins': __dirname + '/src/mixins',
+      'components': __dirname + '/src/components/'
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      loader: 'react-hot!babel-loader'
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.(png|jpg|woff|woff2)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  eslint: {
+    configFile: eslintrcPath
+  }
+};
