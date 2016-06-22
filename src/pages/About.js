7f1afa4a8d3c4e6c7db33b942483f716adf5ba5b
patw <patw.hi@gmail.com>
2016-06-22 16:25:09 +0800

    Convert the About page component as a pure function

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class About extends Component {
-  render() {
-    return (
-      <div className="about">
-        Hi, this is a about page.
-      </div>
-    );
-  }
-}
+const About = () => {
+  return (
+    <div className="about">
+      Hi, this is a about page.
+    </div>
+  );
+};
+
+export default About;
